e50a5162848d63c8303d4158a360502d0d5248b3
QPThree <youngqp3@gmail.com>
2021-05-11 21:27:31 -0700

    object literal for seattle store and method is working in console.  site is live

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 'use strict';
 console.log('Hello World');
+
+
+//1. Create object literals to represent each store
+
+let seattle = {
+  //properties
+  name: 'Seattle',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  cookiesPerCustomer: 6.3,
+  //methods
+  //generates random number of customers per hour
+  calculateCustomersPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
+  }
+
+};
+//2. use method to generate random number of customers per hour
+console.log(seattle.calculateCustomersPerHour());
