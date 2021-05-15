f1f76190db56772d2124a4949850f2f59248879c
QPThree <youngqp3@gmail.com>
2021-05-15 13:38:06 -0700

    constructor function and prototype methods working

--- js/app.js
+++ js/app.js
@@ -1,281 +1,90 @@
 'use strict';
 console.log('Hello World');
 
-//1. Create object literals to represent each store
-let seattle = {
-  //properties
-  name: 'Seattle',
-  minCustomersPerHour: 23,
-  maxCustomersPerHour: 65,
-  cookiesPerCustomer: 6.3,
-  cookiesSoldPerHour: [],
-  //methods
-  //generates random number of customers per hour
-  calculateCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
-  },
-  //function to fill cookies soler per hour array
-  completeDay: function(){
-    for (let i = 0; i < 14; i++){
-      this.cookiesSoldPerHour.push(Math.ceil(this.cookiesPerCustomer * this.calculateCustomersPerHour()));
-    }
-    return this.cookiesSoldPerHour;
-  },
-  maxSold: function(){
-    let output = 0;
-    let hour = '';
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i ++){
-      if (this.cookiesSoldPerHour[i] > output){
-        output = this.cookiesSoldPerHour[i];
-        hour = hoursOpen[i];
-      }
-    }
-    return `${hour}:${output}`;
-  },
-  //publish function
-  publish: function(){
-    this.calculateCustomersPerHour();
-    this.completeDay();
-    let li;
-    let total = 0;
-    let store = document.getElementById(this.name);
-    console.log(store);
-    for (let k = 0; k < hoursOpen.length; k++){
-      li = document.createElement('li');
-      store.appendChild(li);
-      li.innerHTML = `${hoursOpen[k]}: ${this.cookiesSoldPerHour[k]}`;
-      total = total + this.cookiesSoldPerHour[k];
-    }
-    li = document.createElement('li');
-    store.appendChild(li);
-    li.innerHTML = `Total: ${total}`;
-    total = 0;
-    li = '';
-  },
-};
-let tokyo = {
-  //properties
-  name: 'Tokyo',
-  minCustomersPerHour: 3,
-  maxCustomersPerHour: 24,
-  cookiesPerCustomer: 1.2,
-  cookiesSoldPerHour: [],
-  //methods
-  //generates random number of customers per hour
-  calculateCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
-  },
-  //function to fill cookies soler per hour array
-  completeDay: function(){
-    for (let i = 0; i < 14; i++){
-      this.cookiesSoldPerHour.push(Math.ceil(this.cookiesPerCustomer * this.calculateCustomersPerHour()));
-    }
-    return this.cookiesSoldPerHour;
-  },
-  maxSold: function(){
-    let output = 0;
-    let hour = '';
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i ++){
-      if (this.cookiesSoldPerHour[i] > output){
-        output = this.cookiesSoldPerHour[i];
-        hour = hoursOpen[i];
-      }
-    }
-    return `${hour}:${output}`;
-  },
-  //publish function
-  publish: function(){
-    this.calculateCustomersPerHour();
-    this.completeDay();
-    let li;
-    let total = 0;
-    let store = document.getElementById(this.name);
-    console.log(store);
-    for (let k = 0; k < hoursOpen.length; k++){
-      li = document.createElement('li');
-      store.appendChild(li);
-      li.innerHTML = `${hoursOpen[k]}: ${this.cookiesSoldPerHour[k]}`;
-      total = total + this.cookiesSoldPerHour[k];
-    }
-    li = document.createElement('li');
-    store.appendChild(li);
-    li.innerHTML = `Total: ${total}`;
-    total = 0;
-    li = '';
-  },
-};
-let dubai = {
-  //properties
-  name: 'Dubai',
-  minCustomersPerHour: 11,
-  maxCustomersPerHour: 38,
-  cookiesPerCustomer: 3.7,
-  cookiesSoldPerHour: [],
-  //methods
-  //generates random number of customers per hour
-  calculateCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
-  },
-  //function to fill cookies soler per hour array
-  completeDay: function(){
-    for (let i = 0; i < 14; i++){
-      this.cookiesSoldPerHour.push(Math.ceil(this.cookiesPerCustomer * this.calculateCustomersPerHour()));
-    }
-    return this.cookiesSoldPerHour;
-  },
-  maxSold: function(){
-    let output = 0;
-    let hour = '';
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i ++){
-      if (this.cookiesSoldPerHour[i] > output){
-        output = this.cookiesSoldPerHour[i];
-        hour = hoursOpen[i];
-      }
-    }
-    return `${hour}:${output}`;
-  },
-  //publish function
-  publish: function(){
-    this.calculateCustomersPerHour();
-    this.completeDay();
-    let li;
-    let total = 0;
-    let store = document.getElementById(this.name);
-    console.log(store);
-    for (let k = 0; k < hoursOpen.length; k++){
-      li = document.createElement('li');
-      store.appendChild(li);
-      li.innerHTML = `${hoursOpen[k]}: ${this.cookiesSoldPerHour[k]}`;
-      total = total + this.cookiesSoldPerHour[k];
-    }
-    li = document.createElement('li');
-    store.appendChild(li);
-    li.innerHTML = `Total: ${total}`;
-    total = 0;
-    li = '';
-  },
-};
-let paris = {
-  //properties
-  name: 'Paris',
-  minCustomersPerHour: 20,
-  maxCustomersPerHour: 38,
-  cookiesPerCustomer: 2.3,
-  cookiesSoldPerHour: [],
-  //methods
-  //generates random number of customers per hour
-  calculateCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
-  },
-  //function to fill cookies soler per hour array
-  completeDay: function(){
-    for (let i = 0; i < 14; i++){
-      this.cookiesSoldPerHour.push(Math.ceil(this.cookiesPerCustomer * this.calculateCustomersPerHour()));
-    }
-    return this.cookiesSoldPerHour;
-  },
-  maxSold: function(){
-    let output = 0;
-    let hour = '';
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i ++){
-      if (this.cookiesSoldPerHour[i] > output){
-        output = this.cookiesSoldPerHour[i];
-        hour = hoursOpen[i];
-      }
-    }
-    return `${hour}:${output}`;
-  },
-  //publish function
-  publish: function(){
-    this.calculateCustomersPerHour();
-    this.completeDay();
-    let li;
-    let total = 0;
-    let store = document.getElementById(this.name);
-    console.log(store);
-    for (let k = 0; k < hoursOpen.length; k++){
-      li = document.createElement('li');
-      store.appendChild(li);
-      li.innerHTML = `${hoursOpen[k]}: ${this.cookiesSoldPerHour[k]}`;
-      total = total + this.cookiesSoldPerHour[k];
-    }
-    li = document.createElement('li');
-    store.appendChild(li);
-    li.innerHTML = `Total: ${total}`;
-    total = 0;
-    li = '';
-  },
-};
-let lima = {
-  //properties
-  name: 'Lima',
-  minCustomersPerHour: 2,
-  maxCustomersPerHour: 16,
-  cookiesPerCustomer: 4.6,
-  cookiesSoldPerHour: [],
-  //methods
-  //generates random number of customers per hour
-  calculateCustomersPerHour: function() {
-    return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
-  },
-  //function to fill cookies soler per hour array
-  completeDay: function(){
-    for (let i = 0; i < 14; i++){
-      this.cookiesSoldPerHour.push(Math.ceil(this.cookiesPerCustomer * this.calculateCustomersPerHour()));
-    }
-    return this.cookiesSoldPerHour;
-  },
-  maxSold: function(){
-    let output = 0;
-    let hour = '';
-    for (let i = 0; i < this.cookiesSoldPerHour.length; i ++){
-      if (this.cookiesSoldPerHour[i] > output){
-        output = this.cookiesSoldPerHour[i];
-        hour = hoursOpen[i];
-      }
-    }
-    return `${hour}:${output}`;
-  },
-  //publish function
-  publish: function(){
-    this.calculateCustomersPerHour();
-    this.completeDay();
-
-    let li;
-    let total = 0;
-
-    let store = document.getElementById(this.name);
-    console.log(store);
-    for (let k = 0; k < hoursOpen.length; k++){
-      li = document.createElement('li');
-      store.appendChild(li);
-      li.innerHTML = `${hoursOpen[k]}: ${this.cookiesSoldPerHour[k]}`;
-      total = total + this.cookiesSoldPerHour[k];
-    }
-    li = document.createElement('li');
-    store.appendChild(li);
-    li.innerHTML = `Total: ${total}`;
-    total = 0;
-    li = '';
-  },
-};
 //global variables
 const hoursOpen = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-const allStores = [seattle, tokyo, dubai, paris, lima];
+const allStores = [];
 
-//functions
-function publishBestHoursEachStore(){
-  for (let i = 0; i < allStores.length; i++){
-    let e = document.getElementById('totals-max-hour');
-    let li = document.createElement('li');
-    li.innerHTML = `${allStores[i].name}: ${allStores[i].maxSold()}`;
-    e.appendChild(li);
-  }
+
+//constructor function
+
+function CreateStore (name, min, max, cpc){
+  //propertiers from arguments
+  this.name = name;
+  this.minCustomersPerHour = min;
+  this.maxCustomersPerHour = max;
+  this.cookiesPerCustomer = cpc;
+
+  //other properties
+  this.cookiesSoldPerHour = [];
+
+  //run on creation
+  allStores.push(this);
+  this.publish();
+  this.publishBestHour();
+
 }
-function renderAllStores(arr){
-  for (let i = 0; i < arr.length; i++){
-    arr[i].publish();
+
+//prototype methods
+CreateStore.prototype.calculateCustomersPerHour = function(){
+  return Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour);
+};
+
+CreateStore.prototype.completeDay = function(){
+  for (let i = 0; i < 14; i++){
+    this.cookiesSoldPerHour.push(Math.ceil(this.cookiesPerCustomer * this.calculateCustomersPerHour()));
   }
-}
-renderAllStores(allStores);
-publishBestHoursEachStore();
+  return this.cookiesSoldPerHour;
+};
+
+CreateStore.prototype.maxSold = function(){
+  let output = 0;
+  let hour = '';
+  for (let i = 0; i < this.cookiesSoldPerHour.length; i ++){
+    if (this.cookiesSoldPerHour[i] > output){
+      output = this.cookiesSoldPerHour[i];
+      hour = hoursOpen[i];
+    }
+  }
+  return `${hour}:${output}`;
+};
+
+CreateStore.prototype.publish = function(){
+  this.calculateCustomersPerHour();
+  this.completeDay();
+  let li;
+  let total = 0;
+  let store = document.getElementById(this.name);
+  console.log(store);
+  for (let k = 0; k < hoursOpen.length; k++){
+    li = document.createElement('li');
+    store.appendChild(li);
+    li.innerHTML = `${hoursOpen[k]}: ${this.cookiesSoldPerHour[k]}`;
+    total = total + this.cookiesSoldPerHour[k];
+  }
+  li = document.createElement('li');
+  store.appendChild(li);
+  li.innerHTML = `Total: ${total}`;
+  total = 0;
+  li = '';
+};
+
+CreateStore.prototype.publishBestHour = function (){ 
+  let e = document.getElementById('totals-max-hour');
+  let li = document.createElement('li');
+  li.innerHTML = `${this.name}: ${this.maxSold()}`;
+  e.appendChild(li);
+
+};
+
+
+//1. Stores created with constructor baby
+let seattle = new CreateStore('Seattle', 23, 65, 6.3);
+let tokyo = new CreateStore('Tokyo', 3, 24, 1.2);
+let dubai = new CreateStore('Dubai', 11, 38, 3.7);
+let paris = new CreateStore('Paris', 20, 38, 2.3);
+let lima = new CreateStore('Lima', 2, 16, 4.6);
+
+
+
+console.log(allStores);
